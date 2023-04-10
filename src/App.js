3c1ef0e55c47dacd58af6044d74fd8df85fa7bd6
Rifat Ahmed <ahmed1807103@stud.kuet.ac.bd>
2023-04-10 07:24:00 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const questions = [
+		{
+			questionText: 'What is the capital of France?',
+			answerOptions: [
+				{ answerText: 'New York', isCorrect: false },
+				{ answerText: 'London', isCorrect: false },
+				{ answerText: 'Paris', isCorrect: true },
+				{ answerText: 'Dublin', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is CEO of Tesla?',
+			answerOptions: [
+				{ answerText: 'Jeff Bezos', isCorrect: false },
+				{ answerText: 'Elon Musk', isCorrect: true },
+				{ answerText: 'Bill Gates', isCorrect: false },
+				{ answerText: 'Tony Stark', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'The iPhone was created by which company?',
+			answerOptions: [
+				{ answerText: 'Apple', isCorrect: true },
+				{ answerText: 'Intel', isCorrect: false },
+				{ answerText: 'Amazon', isCorrect: false },
+				{ answerText: 'Microsoft', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'How many Harry Potter books are there?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+	];
+
+	const [index, setIndex] = useState(0)
+	const [resReady, setResReady] = useState(false)
+	const [score, setScore] = useState(0)
+	const [prevScore, setPrevScore] = useState(0)
+
+
+	const clicked = (isCorrect)=> {
+		var x = index+1
+		if(isCorrect){
+			setScore(score+1)
+		}
+
+		if(x<questions.length){
+			setIndex(x)
+		}
+		else{
+			setResReady(true)
+		}
+
+	}
+
+	const reset = ()=>{
+		setPrevScore(score);
+		setIndex(0)
+		setResReady(false)
+		setScore(0)
+	}
+	
+
+
+	return (
+		<div className="app">
+			{resReady ? (
+					<>					
+						<div className="score-section"> You got {score} correct out of {questions.length}</div>
+						<div className="prev-score">Previous Score: {prevScore} </div>
+						<button className="trybtn" onClick={()=> reset()}>Try Again</button>
+					</>
+
+
+				) : (
+					<>
+						<div className="question-section">
+							<div className="question-count"><span>Question {index+1}</span>/{questions.length}</div>
+							<div className="question-text">{questions[index].questionText}</div>
+						</div>
+						<div className="answer-section">
+							{questions[index].answerOptions.map((x)=>(
+								<button onClick={()=> clicked(x.isCorrect)}>{x.answerText}</button>
+							))}
+						</div>
+					</>
+				)
+			}
+		</div>
+	);
 }
 
 export default App;
